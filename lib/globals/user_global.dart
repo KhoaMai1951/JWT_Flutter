a8f59478823be698c66f808bbf4d675e59604f58
khoa <chronicle1951@gmail.com>
2021-09-06 19:00:22 +0700

    commit 2

--- lib/globals/user_global.dart
+++ lib/globals/user_global.dart
@@ -5,6 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class UserGlobal {
   static var user;
   static var test;
+  static var test2;
 
   static fetchUserFromLocal() async {
     SharedPreferences localStorage = await SharedPreferences.getInstance();
